fdb1dc6e6bc87ea29b6581b1ba102c9ec78680ba
薛丞宏 <ihcaoe@gmail.com>
2017-01-04 14:25:42 +0800

    會當掠語料落來矣

--- hue7jip8/settings.py
+++ hue7jip8/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    '匯入到臺灣言語資料庫',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
---
+++ 匯入到臺灣言語資料庫/management/commands/族語辭典資料準備.py
@@ -0,0 +1,41 @@
+import json
+from os import makedirs
+from os.path import join
+from random import randint
+from time import sleep
+from urllib.request import urlopen, urlretrieve
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'https://github.com/thewayiam/ami_dict_crawler'
+
+    def handle(self, *args, **參數):
+        語料目錄 = join(settings.BASE_DIR, '族語辭典')
+        makedirs(語料目錄, exist_ok=True)
+        with urlopen(
+            # 'https://github.com/thewayiam/ami_dict_crawler/raw/master/data/data.json'
+            'https://github.com/Taiwanese-Corpus/ami_dict_crawler/raw/master/data/data.json'
+        ) as 資料檔案:
+            資料 = json.loads(資料檔案.read().decode('utf-8'))
+        全部錄音檔 = []
+        for 一筆 in 資料:
+            錄音檔網址 = 一筆['pronounce']
+            if 錄音檔網址 is not None:
+                全部錄音檔.append(錄音檔網址)
+            for 例句 in 一筆['examples']:
+                錄音檔網址 = 例句['pronounce']
+                if 錄音檔網址 is not None:
+                    全部錄音檔.append(錄音檔網址)
+        if len(全部錄音檔) != len(set(全部錄音檔)):
+            raise RuntimeError('有仝網址的音檔')
+        錄音檔對應 = {}
+        for 網址 in 全部錄音檔:
+            sleep(randint(4, 5))
+            所在 = join(語料目錄, 網址.split('/')[-1])
+            urlretrieve(網址, 所在)
+            錄音檔對應[錄音檔網址] = 所在
+        with open(join(語料目錄, '錄音檔對應.json'), 'w') as 檔案:
+            json.dump(錄音檔對應, 檔案, sort_keys=True, indent=2)
