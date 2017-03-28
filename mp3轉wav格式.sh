72b4e21a75ead3805ae673ac48ed1c345a187242
薛丞宏 <ihcaoe@gmail.com>
2017-03-28 18:02:44 +0800

    用python來轉檔

--- mp3轉wav格式.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-function decoding(){
-  mp3_filename=`basename $1`
-  wav_filename="${mp3_filename%.*}".wav
-  avconv -y -i "$1" -vcodec copy -ac 1 -ar 16000 -strict experimental 族語辭典/"$wav_filename";
-}
-export -f decoding
-ls 族語辭典/*.mp3 |\
-  xargs -n 1 -P 4 bash -c 'decoding "$@"' _ 
--- 匯入到臺灣言語資料庫/management/commands/族語辭典1轉檔.py
+++ 匯入到臺灣言語資料庫/management/commands/族語辭典1轉檔.py
@@ -1,8 +1,11 @@
 import json
 from os.path import join
+from posix import listdir
 from urllib.request import urlopen
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
+
 from libavwrapper.avconv import Input, Output, AVConv
 from libavwrapper.codec import AudioCodec, NO_VIDEO
 
@@ -11,12 +14,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **參數):
         # 檢查avconv有裝無
-        網頁聲音格式 = AudioCodec('libmp3lame')
-        網頁聲音格式.channels(1)
-        網頁聲音格式.frequence(16000)
-        網頁聲音格式.bitrate('128k')
-        原始檔案 = Input(self.影音所在())
-        網頁檔案 = Output(所在)
-        指令 = AVConv('avconv', 原始檔案, 網頁聲音格式, NO_VIDEO, 網頁檔案)
-        程序 = 指令.run()
-        結果 = 程序.wait()
+        語料目錄 = join(settings.BASE_DIR, '族語辭典')
+        for 檔名 in sorted(listdir(語料目錄)):
+            if 檔名.endswith('.mp3'):
+                來源 = join(語料目錄, 檔名)
+                目標 = '.'.join(來源.split('.')[:-1]) + '.wav'
+                目標聲音格式 = AudioCodec('pcm_s16le')
+                目標聲音格式.channels(1)
+                目標聲音格式.frequence(16000)
+                原始檔案 = Input(來源)
+                網頁檔案 = Output(目標).overwrite()
+                指令 = AVConv('avconv', 原始檔案, 目標聲音格式, NO_VIDEO, 網頁檔案)
+                程序 = 指令.run()
+                程序.wait()
