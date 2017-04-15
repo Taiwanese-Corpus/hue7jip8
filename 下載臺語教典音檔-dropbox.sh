89aec5383a7a08f7b36e2612af9b795d02f6a380
薛丞宏 <ihcaoe@gmail.com>
2017-04-15 13:28:25 +0800

    轉檔佮掠的指令就莫顯示矣，tox無法度用 >

--- 下載臺語教典音檔-dropbox.sh
+++ 下載臺語教典音檔-dropbox.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 DIR=匯入到臺灣言語資料庫/教育部閩南語常用詞辭典
 wget --directory-prefix=${DIR} https://www.dropbox.com/s/peh3gefzxvores1/twblg_audio_20160926.tar?dl=0
-tar -xvf ${DIR}/twblg_audio_20160926.tar?dl=0 -C ${DIR}
+tar -xf ${DIR}/twblg_audio_20160926.tar?dl=0 -C ${DIR}
--- 臺語教典轉wav格式.sh
+++ 臺語教典轉wav格式.sh
@@ -2,7 +2,7 @@
 function decoding(){
   mp3_filename=`basename $1`
   wav_filename="${mp3_filename%.*}".wav
-  avconv -y -i "$1" -vcodec copy -ac 1 -strict experimental 匯入到臺灣言語資料庫/教育部閩南語常用詞辭典/wav/"$wav_filename";
+  avconv -loglevel error -y -i "$1" -vcodec copy -ac 1 -strict experimental 匯入到臺灣言語資料庫/教育部閩南語常用詞辭典/wav/"$wav_filename";
 }
 mkdir -p 匯入到臺灣言語資料庫/教育部閩南語常用詞辭典/wav
 export -f decoding
