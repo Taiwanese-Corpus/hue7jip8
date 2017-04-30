57bd45c6698e01f675b10c8a146a13d87fb75156
薛丞宏 <ihcaoe@gmail.com>
2017-04-30 10:25:39 +0800

    轉做孤聲道才匯入，HTS才有法度用

--- 匯入到臺灣言語資料庫/management/commands/新北市900例句.py
+++ 匯入到臺灣言語資料庫/management/commands/新北市900例句.py
@@ -1,12 +1,16 @@
 from os import makedirs
-from os.path import join
+from os.path import join, isfile
 from posix import listdir
+from tempfile import TemporaryDirectory
 from urllib.request import urlretrieve
 from zipfile import ZipFile
 
 from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+
+from libavwrapper.avconv import Input, Output, AVConv
+from libavwrapper.codec import AudioCodec, NO_VIDEO
 
 
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
@@ -39,27 +43,37 @@ class Command(BaseCommand):
         網址 = 'https://github.com/Taiwanese-Corpus/Sin1pak8tshi7_2015_900-le7ku3/archive/master.zip'
         語料目錄 = join(settings.BASE_DIR, '語料', '新北市900例句')
         makedirs(語料目錄, exist_ok=True)
-        if len(listdir(語料目錄)) == 0:
-            暫時檔案 = join(語料目錄, 'master.zip')
+        暫時檔案 = join(語料目錄, 'master.zip')
+        if not isfile(暫時檔案):
             urlretrieve(網址, 暫時檔案)
-            ZipFile(暫時檔案).extractall(語料目錄)
-        音檔目錄 = join(語料目錄, 'Sin1pak8tshi7_2015_900-le7ku3-master', '鉸好的1-150音檔')
-        音檔陣列 = []
-        for 檔名 in sorted(listdir(音檔目錄), key=lambda 名: int(名.split('.')[0])):
-            音檔陣列.append(join(音檔目錄, 檔名))
-        with open(
-            join(語料目錄, 'Sin1pak8tshi7_2015_900-le7ku3-master', 'minnan900.分詞')
-        ) as 分詞檔案:
-            for 一逝分詞, 音檔路徑 in zip(分詞檔案.readlines(), 音檔陣列):
-                章物件 = 拆文分析器.分詞章物件(一逝分詞.strip())
-                影音內容 = {'影音所在': 音檔路徑}
-                影音內容.update(self.公家內容)
-                影音 = 影音表.加資料(影音內容)
-                文本內容 = {
-                    '文本資料': 章物件.看型(),
-                    '音標資料': 章物件.看音(),
-                }
-                文本內容.update(self.公家內容)
-                影音.寫文本(文本內容)
+        ZipFile(暫時檔案).extractall(語料目錄)
+        with TemporaryDirectory() as 轉檔目錄:
+            音檔目錄 = join(
+                語料目錄, 'Sin1pak8tshi7_2015_900-le7ku3-master', '鉸好的1-150音檔'
+            )
+            音檔陣列 = []
+            for 檔名 in sorted(listdir(音檔目錄), key=lambda 名: int(名.split('.')[0])):
+                來源 = join(音檔目錄, 檔名)
+                目標 = join(轉檔目錄, 檔名)
+                目標聲音格式 = AudioCodec('pcm_s16le').channels(1)
+                原始檔案 = Input(來源)
+                網頁檔案 = Output(目標).overwrite()
+                指令 = AVConv('avconv', 原始檔案, 目標聲音格式, NO_VIDEO, 網頁檔案)
+                指令.run().wait()
+                音檔陣列.append(目標)
+            with open(
+                join(語料目錄, 'Sin1pak8tshi7_2015_900-le7ku3-master', 'minnan900.分詞')
+            ) as 分詞檔案:
+                for 一逝分詞, 音檔路徑 in zip(分詞檔案.readlines(), 音檔陣列):
+                    章物件 = 拆文分析器.分詞章物件(一逝分詞.strip())
+                    影音內容 = {'影音所在': 音檔路徑}
+                    影音內容.update(self.公家內容)
+                    影音 = 影音表.加資料(影音內容)
+                    文本內容 = {
+                        '文本資料': 章物件.看型(),
+                        '音標資料': 章物件.看音(),
+                    }
+                    文本內容.update(self.公家內容)
+                    影音.寫文本(文本內容)
 
         call_command('顯示資料數量')
