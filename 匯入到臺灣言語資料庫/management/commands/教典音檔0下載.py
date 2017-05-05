0cdc3ee85e5ea5f29886be176014600d77de0fb0
薛丞宏 <ihcaoe@gmail.com>
2017-05-05 13:12:57 +0800

    改用專案路徑

--- 匯入到臺灣言語資料庫/management/commands/教典音檔0下載.py
+++ 匯入到臺灣言語資料庫/management/commands/教典音檔0下載.py
@@ -1,7 +1,7 @@
-from os.path import join
+from os.path import join, dirname, abspath
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
+
 
 from 臺灣言語工具.系統整合.程式腳本 import 程式腳本
 
@@ -16,11 +16,14 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **參數):
+        專案路徑 = join(
+            dirname(abspath(__file__)), '..', '..',
+        )
         腳本路徑 = join(
-            settings.BASE_DIR, '匯入到臺灣言語資料庫', '教育部閩南語常用詞辭典',
+            專案路徑, '匯入到臺灣言語資料庫', '教育部閩南語常用詞辭典',
             '下載臺語教典音檔-{}.sh'.format(參數['下載方式'])
         )
         語料目錄 = join(
-            settings.BASE_DIR, '語料', '教育部閩南語常用詞辭典'
+            專案路徑, '語料', '教育部閩南語常用詞辭典'
         )
         程式腳本._走指令(['bash', 腳本路徑, 語料目錄], 愛直接顯示輸出=True)
