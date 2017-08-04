f2889475f46fc800b0d56fb104a528c6d2065e5c
a8568730 <a8568730@hotmail.com>
2017-08-04 13:54:05 +0800

    改註解

--- 匯入到臺灣言語資料庫/management/commands/台華辭典.py
+++ 匯入到臺灣言語資料庫/management/commands/台華辭典.py
@@ -7,10 +7,10 @@ from django.core.management import call_command
 class Command(BaseCommand):
 
     def handle(self, *args, **參數):
-        # 呼叫語言資料庫的內建函式，顯示各個表格的數量
+        # 呼叫語言資料庫的內建函式，顯示目前各個表格的數量
         call_command('顯示資料數量')
-        # 下載並匯入台華辭典
+        # 下載並匯入台華辭典到語言資料庫
         台華辭典陣列 = 下載()
         匯入(台華辭典陣列)
-        # 呼叫語言資料庫的內建函式，確認各個表格的數量有改變
+        # 呼叫語言資料庫的內建函式，確認外語表和文本表的數量有改變
         call_command('顯示資料數量')
