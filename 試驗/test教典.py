d7130998c53c487ff5be3563bc5d8f96900afec0
a8568730 <a8568730@hotmail.com>
2017-10-11 17:48:53 +0800

    新增 test取得臺羅對照華語

--- 試驗/test教典.py
+++ 試驗/test教典.py
@@ -5,7 +5,7 @@ from 匯入到臺灣言語資料庫.教育部閩南語常用詞辭典.教典 imp
 class 教典試驗(TestCase):
 
     def setUp(self):
-        self.詞目一月日 = {
+        self.一月日詞目 = {
             '主編碼': '6',
             '屬性': '1',
             '詞目': '一月日',
@@ -13,8 +13,31 @@ class 教典試驗(TestCase):
             '文白屬性': '0',
             '部首': '',
         }
+        self.一日月華語 = {
+            'kokgi_no': '9',
+            'n_no': '6',
+            'kokgi': '一個月',
+            'kokgi_v': '一個月',
+        }
+        self.一教典 = 教典()
 
-    def test下載資料正確(self):
-        一教典 = 教典()
-        詞目總檔陣列 = 一教典.下載詞目總檔()
-        self.assertEqual(詞目總檔陣列[4], self.詞目一月日)
+    def test下載詞目正確(self):
+        詞目總檔陣列 = self.一教典.下載詞目總檔()
+        self.assertEqual(詞目總檔陣列[4], self.一月日詞目)
+
+    def test下載華語正確(self):
+        華語檔陣列 = self.一教典.下載對應華語檔()
+        self.assertEqual(華語檔陣列[8], self.一日月華語)
+
+    def test取得臺羅對照華語(self):
+        臺羅華陣列 = self.一教典.取得臺羅對照華語()
+        是否有一日月 = False
+        for 一物件 in 臺羅華陣列:
+            if 一物件.臺字 == '一日月':
+                是否有一日月 = True
+                self.assertEqual(一物件, {
+                    '臺字': '一月日',
+                    '羅馬': 'tsit8 gueh8-jit8',
+                    '華字': '一個月',
+                })
+        self.assertTrue(是否有一日月)
