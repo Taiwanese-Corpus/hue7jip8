e5097b5b6d3893cee3a1cd25c8c469071986b762
fafoy <ihcaoe@gmail.com>
2018-07-18 09:24:51 +0800

    做伙判斷語句字詞

--- 匯入/management/commands/教典詞條.py
+++ 匯入/management/commands/教典詞條.py
@@ -9,6 +9,7 @@ from django.core.management.base import BaseCommand
 
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 臺灣言語服務.models import 訓練過渡格式
+from 匯入.教典 import 字詞抑是語句
 
 
 class Command(BaseCommand):
@@ -16,7 +17,6 @@ class Command(BaseCommand):
 
     公家內容 = {
         '來源': '教典詞條',
-        '種類': '字詞',
         '年代': '2018',
     }
     github網址 = 'https://github.com/g0v/moedict-data-twblg/raw/master/uni/'
@@ -59,6 +59,7 @@ class Command(BaseCommand):
             全部資料.append(
                 訓練過渡格式(
                     文本=句物件.看分詞(),
+                    種類=字詞抑是語句(句物件),
                     **self.公家內容
                 )
             )
