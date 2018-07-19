12f051ebf859d4b74e643df3cec484f0d191e8b4
薛丞宏 <ihcaoe@gmail.com>
2018-07-19 12:18:53 +0800

    全匯

---
+++ 匯入/management/commands/itaigi.py
@@ -0,0 +1,62 @@
+from http.client import HTTPSConnection
+import json
+import ssl
+from urllib.parse import quote
+
+
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from 臺灣言語服務.models import 訓練過渡格式
+from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
+from 臺灣言語工具.解析整理.型態錯誤 import 型態錯誤
+from 匯入.指令 import 匯入枋模
+
+
+class Command(匯入枋模):
+    help = 'https://itaigi.tw'
+    domain = 'db.itaigi.tw'
+    網址 = '/匯出資料'
+
+    公家內容 = {
+        '來源': 'itaigi',
+        '種類': '字詞',
+        '年代': '2018',
+    }
+
+    def 全部資料(self, *args, **參數):
+        全部資料 = []
+        匯入數量 = 0
+        for 一條 in self._全部資料():
+            for 羅馬字 in 一條['羅馬字'].split('/'):
+                try:
+                    台語物件 = 拆文分析器.建立句物件(一條['漢字'], 羅馬字)
+                except 解析錯誤:
+                    print('「{}」「{}」無對齊!'.format(一條['漢字'], 一條['羅馬字']))
+                    continue
+                try:
+                    外文物件 = 拆文分析器.建立句物件(一條['華語'])
+                except 解析錯誤 as 錯誤:
+                    print(錯誤)
+                    continue
+                全部資料.append(
+                    訓練過渡格式(
+                        文本=台語物件.看分詞(),
+                        外文=外文物件.看分詞(),
+                        **self.公家內容
+                    )
+                )
+
+            匯入數量 += 1
+            if 匯入數量 % 1000 == 0:
+                self.stdout.write('匯入 {} 條'.format(匯入數量))
+        return 全部資料
+
+    def _全部資料(self):
+        conn = HTTPSConnection(self.domain)
+        conn.request("GET", quote(self.網址))
+        r1 = conn.getresponse()
+        if r1.status != 200:
+            raise RuntimeError('連線錯誤：{}{}\n{} {}'.format(
+                self.domain, self.網址, r1.status, r1.reason
+            ))
+        內容 = r1.read().decode()
+        return json.loads(內容)['資料']
--- 試驗/testItaigi.py
+++ 試驗/testItaigi.py
@@ -4,16 +4,20 @@ from django.core.management import call_command
 
 
 class itaigi試驗(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        call_command('itaigi')
+        return super(itaigi試驗, cls).setUpClass()
 
     def test句數正確(self):
-        call_command('itaigi')
         self.assertGreater(訓練過渡格式.資料數量(), 8000)
 
-    def test無辭典(self):
-        call_command('itaigi')
+    def test無教典(self):
         self.assertFalse(
-            訓練過渡格式.objects.filter(來源__contains='教典').exitsts()
+            訓練過渡格式.objects.filter(外文='漂-亮', 文本='媠｜suí').exists()
         )
+
+    def test無台華(self):
         self.assertFalse(
-            訓練過渡格式.objects.filter(來源__contains='台華').exitsts()
+            訓練過渡格式.objects.filter(外文='跑-步', 文本='走｜tsáu').exists()
         )
