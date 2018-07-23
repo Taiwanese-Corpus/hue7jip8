0a0640195c1e72a805325209ede95affd50c969a
fafoy <ihcaoe@gmail.com>
2018-07-23 11:57:15 +0800

    因為模組化就免印--ah

--- 匯入/指令.py
+++ 匯入/指令.py
@@ -7,7 +7,6 @@ class 匯入枋模(BaseCommand):
     def handle(self, *args, **參數):
         self.stdout.write('資料數量：{}'.format(訓練過渡格式.資料數量()))
 
-        self.stdout.write('檢查格式了匯入')
         訓練過渡格式.加一堆資料(self.全部資料(*args, **參數))
 
         self.stdout.write('資料數量：{}'.format(訓練過渡格式.資料數量()))
