970d281ae4064bc148782e88d4b0e09b8d7d60c9
薛丞宏 <ihcaoe@gmail.com>
2018-07-24 16:54:40 +0800

    實作了～

--- 匯入/management/commands/台灣植物名彙.py
+++ 匯入/management/commands/台灣植物名彙.py
@@ -13,49 +13,45 @@ class Command(匯入枋模):
     help = 'http://ip194097.ntcu.edu.tw/memory/TGB/thak.asp?id=862'
 
     公家內容 = {
-        '來源': '台灣白話基礎語句',
+        '來源': '台灣植物名彙',
         '種類': '字詞',
-        '年代': '1956',
+        '年代': '1928',
     }
     github網址 = (
         'https://github.com/Taiwanese-Corpus/'
-        'Ko-Chek-hoan-Tan-Pang-tin_1956_Basic-Vocabulary-for-Colloquial-Taiwanese/'
-        'raw/master/ChhoeTaigi_TaioanPehoeKichhooGiku.csv'
+        'Syuniti-Sasaki_1928_List-of-Plants-of-Formosa/'
+        'raw/master/ChhoeTaigi_TaioanSitbutMialui.csv'
     )
 
     def 全部資料(self, *args, **參數):
-        全部資料 = []
         匯入數量 = 0
-        for 羅馬字, 華語 in self.github資料():
-            全部資料.append(
-                訓練過渡格式(
-                    文本=羅馬字,
-                    外文=華語,
-                    **self.公家內容
-                )
+        for 台文 in self.github資料():
+            yield 訓練過渡格式(
+                文本=台文,
+                **self.公家內容
             )
 
             匯入數量 += 1
             if 匯入數量 % 1000 == 0:
                 self.stdout.write('匯入 {} 筆'.format(匯入數量))
-        return 全部資料
 
     def github資料(self):
         with urlopen(self.github網址) as 檔:
             with io.StringIO(檔.read().decode()) as 資料:
                 for row in DictReader(資料):
-                    羅馬字1 = row['poj_unicode'].strip()
-                    羅馬字2 = row['poj_other_unicode'].strip()
-                    華語 = row['hoagi'].strip()
-                    if 羅馬字2 != '':
-                        羅馬字 = [羅馬字1, 羅馬字2]
-                    else:
-                        羅馬字 = [羅馬字1]
-                    for hoa in 華語.split('、'):
-                        for lo in 羅馬字:
-                            try:
-                                台文 = 拆文分析器.建立句物件(lo).看分詞()
-                            except 解析錯誤 as 錯誤:
-                                print(錯誤)
-                            else:
-                                yield 台文, 拆文分析器.建立句物件(hoa).看分詞()
+                    羅馬字 = row['poj_unicode'].strip()
+                    可能漢字 = row['taigi_hanji'].strip()
+                    for lo, han in self.漢羅組合(羅馬字, 可能漢字):
+                        try:
+                            物件 = 拆文分析器.建立句物件(han, lo)
+                        except 解析錯誤 as 錯誤:
+                            self.stderr.write(str(錯誤))
+                        else:
+                            for 字物件 in 物件.篩出字物件():
+                                if 字物件.型 == 'XXX':
+                                    字物件.型 = 字物件.音
+                            yield 物件.看分詞()
+
+    def 漢羅組合(self, 羅馬字, 可能漢字):
+        for 漢字 in 可能漢字.replace('？', ' XXX ').split('、'):
+            yield 羅馬字, 漢字
