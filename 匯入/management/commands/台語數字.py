db20721fdc4a2e71c819457c7c7ad37bccaae28b
Fafofafoy <ihcaoe@gmail.com>
2019-06-19 17:52:33 +0800

    Tso-ho--ah

--- 匯入/management/commands/台語數字.py
+++ 匯入/management/commands/台語數字.py
@@ -13,11 +13,11 @@ from 臺灣言語工具.正規.阿拉伯數字 import 阿拉伯數字
 
 
 class Command(匯入枋模):
-    help = 'https://github.com/g0v/moedict-data-twblg/'
-    例句網址 = 'https://github.com/g0v/moedict-data-twblg/raw/master/uni/%E4%BE%8B%E5%8F%A5.csv'
+    help = 'https://github.com/Taiwanese-Corpus/TaiOanUe-LiLiKhokKhok-SuPio'
+    資料網址 = 'https://github.com/Taiwanese-Corpus/TaiOanUe-LiLiKhokKhok-SuPio/raw/master/sooji.csv'
 
     公家內容 = {
-        '種類':'字詞',
+        '種類': '字詞',
         '來源': '台語數字',
         '年代': '2019',
     }
@@ -33,15 +33,9 @@ class Command(匯入枋模):
             匯入數量 += 1
             if 匯入數量 % 1000 == 0:
                 self.stdout.write('匯入 {} 筆'.format(匯入數量))
+
     def _全部資料(self):
-        a = []
-        for sooji in range(11,100):
-            a.append(阿拉伯數字().轉數量('空', str(sooji)))
-        print(a)
-#         return [
-#             拆文分析器.建立句物件('百外', 'pah-guā'),
-#             拆文分析器.建立句物件('百sann', 'pah-sann'),
-#             ]
-        
-        yield 拆文分析器.建立句物件(a,'pah-guā')
-        yield 拆文分析器.建立句物件('百sann', 'pah-sann')
+        with urlopen(self.資料網址) as tsuliau:
+            with io.StringIO(tsuliau.read().decode('utf-8')) as tong:
+                for pit in DictReader(tong):
+                    yield 拆文分析器.建立句物件(pit['漢字'], pit['羅馬字'])
